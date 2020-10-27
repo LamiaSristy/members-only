5b892eeb7d2b02d08064e100ea387a26456262f9
NtwaliHeritier <heritierntwali@gmail.com>
2020-10-27 10:53:56 +0200

    add the post model and controller

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,8 @@
+class PostsController < ApplicationController
+    before_action :authenticate_user!, only: [:new, :create]
+
+    def new
+        @post = Post.new
+    end
+    
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_27_081514) do
+ActiveRecord::Schema.define(version: 2020_10_27_084515) do
+
+  create_table "posts", force: :cascade do |t|
+    t.string "title"
+    t.text "body"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
