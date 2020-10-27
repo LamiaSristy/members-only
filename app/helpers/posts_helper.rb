b646c2e01f88d408b7ca4f94901413334f0ea444
NtwaliHeritier <heritierntwali@gmail.com>
2020-10-27 14:28:57 +0200

    add logic in view helpers

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,2 +1,14 @@
 module PostsHelper
+    def check_signed
+        if user_signed_in?
+            # <%= link_to "Create a New Post", new_post_path %>
+            "<a href='/posts/new'>Create a new post</a>".html_safe
+        end
+    end
+
+    def display_user_email(post)
+        if user_signed_in?   
+            "<p>#{post.user.email}</p>".html_safe
+         end
+    end
 end
