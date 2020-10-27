b92915b2286f60f3295b8fb1469f14eaaa653149
lamiasristy <lamiasristy@gmail.com>
2020-10-27 10:39:47 +0100

    Added the create method in controller and  README.md file.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,8 +1,27 @@
 class PostsController < ApplicationController
-    before_action :authenticate_user!, only: [:new, :create]
+  before_action :authenticate_user!, only: %i[new create]
 
-    def new
-        @post = Post.new
+  def index 
+    @posts = Post.all
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    @post.user = current_user
+    if @post.save
+      redirect_to posts_path
+    else
+      render :new
     end
-    
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :body)
+  end
 end
